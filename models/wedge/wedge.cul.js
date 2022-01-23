9d00bf90cb6ea2262745823977835045145e140e
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-01-23 19:44:30 +0000

    some wedge dev for forces and changing coordinate systems

--- models/wedge/wedge.cul.js
+++ models/wedge/wedge.cul.js
@@ -8,12 +8,34 @@
 // w/o friction the net force on inclined plane is mg*sin(theta) [mg=weight or gravitational force]
 // => a = F/m = g*sin(theta) [along the inclined plane]
 
-//export const weight = () => (m() * g());
-// ...
+// todo generalise rotation of (0, mg) force? (think rotation matrix)
 
-export const a = () => g() * Math.sin(theta()); // relative to inclined plane, no coord stuff happening yet
+export const weight_y = () => {
+  if (coords() == 'ground') return m() * g();
+  else if (coords() == 'plane')
+    return weight_y({ coords_in: 'ground' }) * Math.cos(theta());
+};
+
+export const weight_x = () => {
+  if (coords() == 'ground') return 0;
+  else if (coords() == 'plane')
+    return weight_y(/*!*/ { coords_in: 'ground' }) * Math.sin(theta());
+};
+
+// other forces: Normal reaction which offsets weight_y on 'plane'
+// friction(out)
+
+// => net force on 'plane' is (?, 0)
+// but on 'ground' is (?, ?)
+
+export const a_x = () => 1; // todo
+
+export const a_y = () => 1; // todo
+
+//export const a = () => g() * Math.sin(theta()); // relative to inclined plane, no coord stuff happening yet
 
 export const coords = () => coords_in; // 'plane' for inclined plane and 'ground'
 export const theta = () => theta_in; // radians, 1 radian ~= 57 degrees, todo configurable
 export const g = () => g_in;
 export const m = () => m_in; // force depends on mass, but not accelaration
+// todo also generalise x,y directions
