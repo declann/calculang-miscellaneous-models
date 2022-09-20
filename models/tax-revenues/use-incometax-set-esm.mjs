875a5c749125c6cac7ffd5e292006cecda1bb893
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-09-20 20:09:08 +0100

    use script

--- models/tax-revenues/use-incometax-set-esm.mjs
+++ models/tax-revenues/use-incometax-set-esm.mjs
@@ -1,13 +1,17 @@
 import * as m from './incometax-set_esm/cul_scope_0.mjs';
 import { range } from 'underscore';
 
-console.log(
-  m.income_tax_sum({
-    taxpayer_table_in: [
-      {
-        taxpayer_id: 0,
-        gross_salary: 10000,
-      },
-    ],
-  })
-);
+const taxpayer_table_in = [
+  {
+    taxpayer_id: 0,
+    gross_salary: 10000,
+  },
+  {
+    taxpayer_id: 1,
+    gross_salary: 20000,
+  },
+];
+
+console.log(m.income_tax_sum({ taxpayer_table_in }));
+
+console.log(m.income_tax({ taxpayer_table_in, taxpayer_id_in: 0 }));
