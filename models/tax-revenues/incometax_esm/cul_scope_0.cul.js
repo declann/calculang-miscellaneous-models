b1375635d00b822226f52d13a8e34e86c4032d22
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-09-20 19:50:15 +0100

    incometax -> simple-incometax

--- models/tax-revenues/incometax_esm/cul_scope_0.cul.js
+++
@@ -1,9 +0,0 @@
-// a simple one
-
-export const net_salary = () => gross_salary() - income_tax();
-
-export const gross_salary = () => gross_salary_in;
-
-export const income_tax = () => Math.min(0.1 * gross_salary() - tax_credit(), 0);
-
-export const tax_credit = () => (5000);
