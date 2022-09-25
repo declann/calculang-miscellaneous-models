9687fb3ee84dd01e96a33c3b56c884bd8a5d11df
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-09-25 12:56:08 +0100

    taxpayer_count scaling factor

--- models/tax-revenues/impact_esm/cul_scope_1.cul.js
+++ models/tax-revenues/impact_esm/cul_scope_1.cul.js
@@ -3,24 +3,37 @@ export { income_tax, effective_rate, usc, paye };
 
 export const taxpayer_id = () => taxpayer_id_in;
 export const taxpayer_table = () => taxpayer_table_in;
+export const taxpayer_count = () => taxpayer_count_in;
 
 export const gross_salary = () => taxpayer_table()[taxpayer_id()].gross_salary;
 export const proportion = () => taxpayer_table()[taxpayer_id()].proportion;
 
 export const income_tax_sum = () =>
   taxpayer_table().reduce(
-    (acc, val) => acc + income_tax({ taxpayer_id_in: val.taxpayer_id }) * proportion({ taxpayer_id_in: val.taxpayer_id }),
+    (acc, val) =>
+      acc +
+      income_tax({ taxpayer_id_in: val.taxpayer_id }) *
+        proportion({ taxpayer_id_in: val.taxpayer_id }) *
+        taxpayer_count(),
     0
   );
 
 export const paye_sum = () =>
   taxpayer_table().reduce(
-    (acc, val) => acc + paye({ taxpayer_id_in: val.taxpayer_id }) * proportion({ taxpayer_id_in: val.taxpayer_id }),
+    (acc, val) =>
+      acc +
+      paye({ taxpayer_id_in: val.taxpayer_id }) *
+        proportion({ taxpayer_id_in: val.taxpayer_id }) *
+        taxpayer_count(),
     0
   );
 
 export const usc_sum = () =>
   taxpayer_table().reduce(
-    (acc, val) => acc + usc({ taxpayer_id_in: val.taxpayer_id }) * proportion({ taxpayer_id_in: val.taxpayer_id }),
+    (acc, val) =>
+      acc +
+      usc({ taxpayer_id_in: val.taxpayer_id }) *
+        proportion({ taxpayer_id_in: val.taxpayer_id }) *
+        taxpayer_count(),
     0
   );
