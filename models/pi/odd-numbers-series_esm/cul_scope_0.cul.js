6b18c05e3a95e5a45f2dbb1a7379f9761e45db03
Declan Naughton <declan.naughton2@mail.dcu.ie>
2023-03-14 18:26:38 -0300

    odd-number-series : some cleanup

--- models/pi/odd-numbers-series_esm/cul_scope_0.cul.js
+++ models/pi/odd-numbers-series_esm/cul_scope_0.cul.js
@@ -6,21 +6,31 @@ import { range } from 'underscore'
 // 1/1 - 1/3 + 1/5 - 1/7 + ... = pi/4
 // starting at 1 i.e. term 1 = 1/1, 2=-1/3, ...
 
-export const term_number = () => term_number_in;
-export const terms = () => terms_in;
 
-export const sign = () => Math.pow(-1, term_number() - 1); // +, -, +, -, ...
+// alternating sign: +, -, +, -, ...
+export const sign = () => Math.pow(-1, term_number() - 1);
 
-export const denominator = () => (term_number() - 1) * 2 + 1; // odd number series
+// denominator from sequence of odd numbers
+export const denominator = () => (term_number() - 1) * 2 + 1;
 
+// input: number of current term
+export const term_number = () => term_number_in; 
+
+// one term:
 export const term = () => sign() * (1 / denominator());
-/*
+
+// sum of terms (1 to terms input):
 export const sum_of_terms = () => range(1, terms()).reduce(
   (acc, term_number_in) => acc + term({ term_number_in }),
   0);
-*/
 
-export const sum_of_terms = () => {
+// pi approximation:
+export const pi_approximation = () => sum_of_terms() * 4;
+
+// input: total number of terms to sum
+export const terms = () => terms_in;
+
+/*export const sum_of_terms = () => {
   let res = 0, term_number_in = 1;
   
   while(term_number_in < terms()) {
@@ -29,24 +39,4 @@ export const sum_of_terms = () => {
   }
 
   return res
-  }
-
-export const sum_of_terms_times_4 = () => sum_of_terms() * 4
-
-
-// abstract this ...
-export const series_sum_of_first_10_terms = () =>
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce(
-    (acc, term_number_in) => acc + term({ term_number_in }),
-    0
-  );
-
-export const series_sum_of_first_9_terms = () =>
-  [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce(
-    (acc, term_number_in) => acc + term({ term_number_in }),
-    0
-  );
-
-
-
-// 0.76045990473235055278398931649706
+  }*/
--- models/pi/odd-numbers-series_esm/cul_scope_0.mjs
+++ models/pi/odd-numbers-series_esm/cul_scope_0.mjs
@@ -6,47 +6,37 @@ import { range } from 'underscore';
 // 1/1 - 1/3 + 1/5 - 1/7 + ... = pi/4
 // starting at 1 i.e. term 1 = 1/1, 2=-1/3, ...
 
+
+// alternating sign: +, -, +, -, ...
+export const sign = ({ term_number_in }) => Math.pow(-1, term_number({ term_number_in }) - 1);
+
+// denominator from sequence of odd numbers
+export const denominator = ({ term_number_in }) => (term_number({ term_number_in }) - 1) * 2 + 1;
+
+// input: number of current term
 export const term_number = ({ term_number_in }) => term_number_in;
+
+// one term:
+export const term = ({ term_number_in }) => sign({ term_number_in }) * (1 / denominator({ term_number_in }));
+
+// sum of terms (1 to terms input):
+export const sum_of_terms = ({ terms_in }) => range(1, terms({ terms_in })).reduce(
+(acc, term_number_in) => acc + term({ term_number_in }),
+0);
+
+// pi approximation:
+export const pi_approximation = ({ terms_in }) => sum_of_terms({ terms_in }) * 4;
+
+// input: total number of terms to sum
 export const terms = ({ terms_in }) => terms_in;
 
-export const sign = ({ term_number_in }) => Math.pow(-1, term_number({ term_number_in }) - 1); // +, -, +, -, ...
-
-export const denominator = ({ term_number_in }) => (term_number({ term_number_in }) - 1) * 2 + 1; // odd number series
-
-export const term = ({ term_number_in }) => sign({ term_number_in }) * (1 / denominator({ term_number_in }));
-/*
-export const sum_of_terms = () => range(1, terms()).reduce(
-  (acc, term_number_in) => acc + term({ term_number_in }),
-  0);
-*/
-
-export const sum_of_terms = ({ terms_in }) => {
-  let res = 0,term_number_in = 1;
-
-  while (term_number_in < terms({ terms_in })) {
-    res += term({ term_number_in });
+/*export const sum_of_terms = () => {
+  let res = 0, term_number_in = 1;
+  
+  while(term_number_in < terms()) {
+    res += term({term_number_in})
     term_number_in++;
   }
 
-  return res;
-};
-
-export const sum_of_terms_times_4 = ({ terms_in }) => sum_of_terms({ terms_in }) * 4;
-
-
-// abstract this ...
-export const series_sum_of_first_10_terms = ({}) =>
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce(
-(acc, term_number_in) => acc + term({ term_number_in }),
-0);
-
-
-export const series_sum_of_first_9_terms = ({}) =>
-[1, 2, 3, 4, 5, 6, 7, 8, 9].reduce(
-(acc, term_number_in) => acc + term({ term_number_in }),
-0);
-
-
-
-
-// 0.76045990473235055278398931649706
+  return res
+  }*/
