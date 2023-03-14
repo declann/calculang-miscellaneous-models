2bfc8047284103454556f3fd526efee021a6e5ef
Declan Naughton <declan.naughton2@mail.dcu.ie>
2023-03-14 15:59:21 -0300

    comment on results

--- models/pi/use-nilakantha-series-bignumber.mjs
+++ models/pi/use-nilakantha-series-bignumber.mjs
@@ -1,7 +1,14 @@
 import {
-  pi_approximation, sum_of_terms
+    pi_approximation, sum_of_terms
 } from './nilakantha-series-bignumber_esm/cul_scope_0.mjs';
 
-console.log(sum_of_terms({terms_in:1}).toFixed())
-console.log(pi_approximation({terms_in:1}).toFixed())
-console.log(pi_approximation({terms_in:1e6}).toFixed()) // 
+console.log(sum_of_terms({ terms_in: 1 }).toFixed());
+console.log(pi_approximation({ terms_in: 1 }).toFixed());
+console.log(pi_approximation({ terms_in: 1e6 }).toFixed());
+
+// ans using 1e6 (1M) terms:
+
+// 3.1415926535897932387126433832792017957459452741092124882086541815807967314969323542518228257404621996
+// vs 
+// 3.141592653589793238462643383279
+// 3.141592653589793238 <-- this part is the same
