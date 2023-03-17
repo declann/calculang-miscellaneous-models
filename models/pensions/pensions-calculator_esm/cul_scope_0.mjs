d6537a484f2e1d83dcaa9f29e2b41c863ab5d0d5
Declan Naughton <declan.naughton2@mail.dcu.ie>
2023-03-17 13:21:03 -0300

    push pensions calculator example

---
+++ models/pensions/pensions-calculator_esm/cul_scope_0.mjs
@@ -0,0 +1,186 @@
+
+import { memoize } from 'underscore';
+//import memoize from 'lru-memoize';
+//import { isEqual } from 'underscore'; // TODO poor tree shaking support, or why is this impact so massive? Move to lodash/lodash-es?
+
+import { fund_value_ as fund_value$, unit_balance_ as unit_balance$, unit_allocation_ as unit_allocation$, unit_price_ as unit_price$, annual_premium_ as annual_premium$, annual_salary_ as annual_salary$, projected_fund_value_ as projected_fund_value$, age_ as age$, age_0_ as age_0$, retirement_age_ as retirement_age$, annual_salary_0_ as annual_salary_0$, salary_inflation_rate_ as salary_inflation_rate$, empee_contribution_rate_ as empee_contribution_rate$, unit_growth_rate_ as unit_growth_rate$, fund_value_0_ as fund_value_0$ } from "./cul_scope_1.mjs"; // there is already-culed stuff in here, why? imports to memo loader include cul_scope_id, what logic should it apply RE passing forward? eliminate? Probably!
+
+
+
+////////// start fund_value memo-loader code //////////
+//const fund_value$m = memoize(999999, isEqual)(fund_value$);
+export const fund_value$m = memoize(fund_value$, JSON.stringify);
+export const fund_value = (a) => {
+  return fund_value$m(a);
+  // eslint-disable-next-line no-undef
+  fund_value$({ age_in, age_0_in, fund_value_0_in, unit_growth_rate_in, retirement_age_in, annual_salary_0_in, salary_inflation_rate_in, empee_contribution_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end fund_value memo-loader code //////////
+
+
+
+////////// start unit_balance memo-loader code //////////
+//const unit_balance$m = memoize(999999, isEqual)(unit_balance$);
+export const unit_balance$m = memoize(unit_balance$, JSON.stringify);
+export const unit_balance = (a) => {
+  return unit_balance$m(a);
+  // eslint-disable-next-line no-undef
+  unit_balance$({ age_in, age_0_in, fund_value_0_in, unit_growth_rate_in, retirement_age_in, annual_salary_0_in, salary_inflation_rate_in, empee_contribution_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end unit_balance memo-loader code //////////
+
+
+
+////////// start unit_allocation memo-loader code //////////
+//const unit_allocation$m = memoize(999999, isEqual)(unit_allocation$);
+export const unit_allocation$m = memoize(unit_allocation$, JSON.stringify);
+export const unit_allocation = (a) => {
+  return unit_allocation$m(a);
+  // eslint-disable-next-line no-undef
+  unit_allocation$({ age_in, age_0_in, retirement_age_in, annual_salary_0_in, salary_inflation_rate_in, empee_contribution_rate_in, unit_growth_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end unit_allocation memo-loader code //////////
+
+
+
+////////// start unit_price memo-loader code //////////
+//const unit_price$m = memoize(999999, isEqual)(unit_price$);
+export const unit_price$m = memoize(unit_price$, JSON.stringify);
+export const unit_price = (a) => {
+  return unit_price$m(a);
+  // eslint-disable-next-line no-undef
+  unit_price$({ age_in, age_0_in, unit_growth_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end unit_price memo-loader code //////////
+
+
+
+////////// start annual_premium memo-loader code //////////
+//const annual_premium$m = memoize(999999, isEqual)(annual_premium$);
+export const annual_premium$m = memoize(annual_premium$, JSON.stringify);
+export const annual_premium = (a) => {
+  return annual_premium$m(a);
+  // eslint-disable-next-line no-undef
+  annual_premium$({ age_in, age_0_in, retirement_age_in, annual_salary_0_in, salary_inflation_rate_in, empee_contribution_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end annual_premium memo-loader code //////////
+
+
+
+////////// start annual_salary memo-loader code //////////
+//const annual_salary$m = memoize(999999, isEqual)(annual_salary$);
+export const annual_salary$m = memoize(annual_salary$, JSON.stringify);
+export const annual_salary = (a) => {
+  return annual_salary$m(a);
+  // eslint-disable-next-line no-undef
+  annual_salary$({ age_in, age_0_in, annual_salary_0_in, retirement_age_in, salary_inflation_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end annual_salary memo-loader code //////////
+
+
+
+////////// start projected_fund_value memo-loader code //////////
+//const projected_fund_value$m = memoize(999999, isEqual)(projected_fund_value$);
+export const projected_fund_value$m = memoize(projected_fund_value$, JSON.stringify);
+export const projected_fund_value = (a) => {
+  return projected_fund_value$m(a);
+  // eslint-disable-next-line no-undef
+  projected_fund_value$({ age_0_in, fund_value_0_in, unit_growth_rate_in, retirement_age_in, annual_salary_0_in, salary_inflation_rate_in, empee_contribution_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end projected_fund_value memo-loader code //////////
+
+
+
+////////// start age memo-loader code //////////
+//const age$m = memoize(999999, isEqual)(age$);
+export const age$m = memoize(age$, JSON.stringify);
+export const age = (a) => {
+  return age$m(a);
+  // eslint-disable-next-line no-undef
+  age$({ age_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end age memo-loader code //////////
+
+
+
+////////// start age_0 memo-loader code //////////
+//const age_0$m = memoize(999999, isEqual)(age_0$);
+export const age_0$m = memoize(age_0$, JSON.stringify);
+export const age_0 = (a) => {
+  return age_0$m(a);
+  // eslint-disable-next-line no-undef
+  age_0$({ age_0_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end age_0 memo-loader code //////////
+
+
+
+////////// start retirement_age memo-loader code //////////
+//const retirement_age$m = memoize(999999, isEqual)(retirement_age$);
+export const retirement_age$m = memoize(retirement_age$, JSON.stringify);
+export const retirement_age = (a) => {
+  return retirement_age$m(a);
+  // eslint-disable-next-line no-undef
+  retirement_age$({ retirement_age_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end retirement_age memo-loader code //////////
+
+
+
+////////// start annual_salary_0 memo-loader code //////////
+//const annual_salary_0$m = memoize(999999, isEqual)(annual_salary_0$);
+export const annual_salary_0$m = memoize(annual_salary_0$, JSON.stringify);
+export const annual_salary_0 = (a) => {
+  return annual_salary_0$m(a);
+  // eslint-disable-next-line no-undef
+  annual_salary_0$({ annual_salary_0_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end annual_salary_0 memo-loader code //////////
+
+
+
+////////// start salary_inflation_rate memo-loader code //////////
+//const salary_inflation_rate$m = memoize(999999, isEqual)(salary_inflation_rate$);
+export const salary_inflation_rate$m = memoize(salary_inflation_rate$, JSON.stringify);
+export const salary_inflation_rate = (a) => {
+  return salary_inflation_rate$m(a);
+  // eslint-disable-next-line no-undef
+  salary_inflation_rate$({ salary_inflation_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end salary_inflation_rate memo-loader code //////////
+
+
+
+////////// start empee_contribution_rate memo-loader code //////////
+//const empee_contribution_rate$m = memoize(999999, isEqual)(empee_contribution_rate$);
+export const empee_contribution_rate$m = memoize(empee_contribution_rate$, JSON.stringify);
+export const empee_contribution_rate = (a) => {
+  return empee_contribution_rate$m(a);
+  // eslint-disable-next-line no-undef
+  empee_contribution_rate$({ empee_contribution_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end empee_contribution_rate memo-loader code //////////
+
+
+
+////////// start unit_growth_rate memo-loader code //////////
+//const unit_growth_rate$m = memoize(999999, isEqual)(unit_growth_rate$);
+export const unit_growth_rate$m = memoize(unit_growth_rate$, JSON.stringify);
+export const unit_growth_rate = (a) => {
+  return unit_growth_rate$m(a);
+  // eslint-disable-next-line no-undef
+  unit_growth_rate$({ unit_growth_rate_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end unit_growth_rate memo-loader code //////////
+
+
+
+////////// start fund_value_0 memo-loader code //////////
+//const fund_value_0$m = memoize(999999, isEqual)(fund_value_0$);
+export const fund_value_0$m = memoize(fund_value_0$, JSON.stringify);
+export const fund_value_0 = (a) => {
+  return fund_value_0$m(a);
+  // eslint-disable-next-line no-undef
+  fund_value_0$({ fund_value_0_in }); // never run, but here to "trick" calculang graph logic
+};
+////////// end fund_value_0 memo-loader code //////////
---
+++ models/pensions/use-pensions-calculator.mjs
@@ -0,0 +1,78 @@
+import { annual_premium, annual_salary, fund_value, unit_allocation, unit_balance, unit_price } from './pensions-calculator_esm/cul_scope_0.mjs';
+
+import { range } from 'underscore';
+
+let ins = {
+    //age_in: 60,
+    age_0_in: 40,
+    fund_value_0_in: 100000,
+    unit_growth_rate_in: 0.05,
+    retirement_age_in: 65,
+    annual_salary_0_in: 100000,
+    salary_inflation_rate_in: 0.04,
+    empee_contribution_rate_in: 0.1
+};
+
+/*let data = [annual_premium, annual_salary, fund_value, unit_allocation, unit_balance, unit_price].map(fn => )
+.flat()*/
+
+
+//console.table(data);
+
+console.log(JSON.stringify(data, null, 2));
+
+console.log([annual_premium])
+
+// from spec.model and spec.input_domains generate data
+
+// https://stackoverflow.com/questions/18957972/cartesian-product-of-objects-in-javascript
+function copy(obj) {
+    var res = {};
+    for (var p in obj) res[p] = obj[p];
+    return res;
+}
+
+// https://stackoverflow.com/questions/18957972/cartesian-product-of-objects-in-javascript
+function cartesianProduct(input, current) {
+    if (!input || !input.length) {
+        return [];
+    }
+
+    var head = input[0];
+    var tail = input.slice(1);
+    var output = [];
+
+    for (var key in head) {
+        for (var i = 0; i < head[key].length; i++) {
+            var newCurrent = copy(current);
+            newCurrent[key] = head[key][i];
+            if (tail.length) {
+                var productOfTail = cartesianProduct(tail, newCurrent);
+                output = output.concat(productOfTail);
+            } else output.push(newCurrent);
+        }
+    }
+    return output;
+}
+
+// I still need z.cursor.... now values in input_domains
+// I also need to process the encoding-mapped fields
+
+/*
+input_domains_A_projection = {
+  var o = {}
+  Object.entries(z.cursor).forEach(([k,v]) => {
+    o[k] = [v]
+  })
+  Object.entries(input_domains_A_overrides).forEach(([k,v]) => {
+    o[k] = v
+  })
+  //o.formula = Object.values(model)
+  return o
+}*/
+
+//////input_combos_A_projection = cartesianProduct(Object.entries(input_domains_A_projection).map(([k,v]) => ({[k]: v})))
+
+// age_in bit here not done programmatically ! just use all combos?
+//////projection = formulae_info.filter(d => formulae_info_inputs.indexOf(d.name+'_in') == -1).map(({name}) => input_combos_A_projection.map(combos => ({...combos,/*[pvt_y]: combos[pvt_y],*/ formula:name, /*[name]*/value: +model[name](combos)/*.toFixed(2)*/}))).flat()
+// pre-folded!
