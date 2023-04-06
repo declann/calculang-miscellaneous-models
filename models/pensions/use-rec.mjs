edc5e494143f74b96f600aaca3135b72e1feb6be
Declan Naughton <declan.naughton2@mail.dcu.ie>
2023-04-06 15:41:53 -0300

    some rec values

--- models/pensions/use-rec.mjs
+++ models/pensions/use-rec.mjs
@@ -1,3 +1,5 @@
 import { fund_value } from "./rec_esm/cul_scope_0.mjs";
 
-console.log(fund_value({ age_in: 21, rec_step_in: 0, age_opening_in: 20, age_closing_in: 21 }))
+console.log(fund_value({ age_in: 22, rec_step_in: 0, age_opening_in: 20, age_closing_in: 21 }))
+console.log(fund_value({ age_in: 22, rec_step_in: 1, age_opening_in: 20, age_closing_in: 21 }))
+console.log(fund_value({ age_in: 22, rec_step_in: 2, age_opening_in: 20, age_closing_in: 21 }))
