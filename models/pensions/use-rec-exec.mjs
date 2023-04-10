158d475e87ab9960ff63bbb85eeb4f4e9d47dda0
Declan Naughton <declan.naughton2@mail.dcu.ie>
2023-04-10 17:12:30 -0300

    1000 year projections working without memo :confused: !

--- models/pensions/use-rec-exec.mjs
+++ models/pensions/use-rec-exec.mjs
@@ -3,3 +3,4 @@ import { fund_value } from "./rec-exec_esm/cul_scope_0.mjs";
 console.log(fund_value({ age_in: 22, rec_step_in: 0, age_opening_in: 20, age_closing_in: 21 }))
 console.log(fund_value({ age_in: 22, rec_step_in: 1, age_opening_in: 20, age_closing_in: 21 }))
 console.log(fund_value({ age_in: 22, rec_step_in: 2, age_opening_in: 20, age_closing_in: 21 }))
+console.log(fund_value({ age_in: 1000, rec_step_in: 0, age_opening_in: 20, age_closing_in: 20 }))
