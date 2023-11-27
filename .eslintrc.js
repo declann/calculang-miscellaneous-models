51e287b729368f2ba38b1aeeb3ed19cab211b034
Declan Naughton <declann1990@gmail.com>
2023-11-27 16:11:02 +0000

    eslint change #126

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,15 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  ignorePatterns: ['**/*.cul.js'], // how to ignore bundles? or just add define as a global?
+  //ignorePatterns: ['**/*.cul.js'], // how to ignore bundles? or just add define as a global?
   rules: {
     'no-undef': ['error'/*, { "globals": { "define": "readonly" } }*/], // some version of eslint don't like the global define !
     'no-restricted-syntax': [
       'error',
       {
-        message: "calculang: don't pollute the _ namespace",
+        message: "calculang: don't pollute the _ namespace, 'as' to something not ending with _",
         selector:
-          'ImportDeclaration[source.value=/cul_scope_/] > ImportSpecifier[local.name=/_$/]',
+          'ImportDeclaration[source.value=/cul\.js/] > ImportSpecifier[local.name=/_$/]',
         // converted to esm => match cul_scope_x rather than .cul
       },
     ],
